79a72a848f2dc7c6bfdc19a1e37e481de4f4a03d
zhangyuan <13584851153@163.com>
2023-07-07 09:55:15 +0800

    update java 1.8 to 11

--- IotLib/lib_camera/src/androidTest/java/com/zy/camera/ExampleInstrumentedTest.kt
+++ IotLib/lib_camera/src/androidTest/java/com/zy/camera/ExampleInstrumentedTest.kt
@@ -1,12 +1,9 @@
 package com.zy.camera
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
